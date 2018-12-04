a0c093c8058da96eb46ce6e83faeec90221819bd
Mattias Jansson <mattias.jansson@adabeat.com>
2018-12-04 12:38:10 +0100

    Mattias: Minor improvement on day1

--- Day01/Part2.hs
+++ Day01/Part2.hs
@@ -1,11 +1,11 @@
 import qualified Data.Set as S
 
-main = interact (show . search . parse . cycle)
+main = interact (show . search . scanl1' (+) . parse . cycle)
 
 parse = map read . lines . filter (/= '+')
 
 search = loop 0 S.empty
-  where loop current reached (change:changes)
+  where loop reached (change:changes)
           | S.member current reached = current
           | otherwise = let next = current + change
                         in loop next (S.insert current reached) changes
